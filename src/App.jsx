c3cc350daf53dadf90930f1cabd3179b2f534e3b
colinepiou <coline.piou@gmail.com>
2026-06-07 13:26:05 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
-import { Routes, Route } from 'react-router-dom'
-import Layout from './components/Layout.jsx'
+import React from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import Layout from './components/Layout'
 import Accueil from './pages/Accueil.jsx'
 import QuiSuisJe from './pages/QuiSuisJe.jsx'
 import Contact from './pages/Contact.jsx'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  React.useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
 export default function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route path="/"          element={<Accueil />} />
-        <Route path="/qui-suis-je" element={<QuiSuisJe />} />
-        <Route path="/contact"   element={<Contact />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/"            element={<Accueil />} />
+          <Route path="/qui-suis-je" element={<QuiSuisJe />} />
+          <Route path="/contact"     element={<Contact />} />
+        </Route>
+      </Routes>
+    </>
   )
 }
